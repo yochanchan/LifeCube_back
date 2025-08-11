0bca4b276bb2285841343b9a501b3c4c0cc0a583
yochanchan <arai0201@gmail.com>
2025-08-11 16:05:55 +0900

    DB部分を微修正

--- api/pictures.py
+++ api/pictures.py
@@ -33,6 +33,10 @@ def get_db():
     db = SessionLocal()
     try:
         yield db
+        db.commit()
+    except Exception:
+        db.rollback()
+        raise
     finally:
         db.close()
 
